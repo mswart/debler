a28c121b66e3778ede842e5ecca7c2f7dcf1b43e
Malte Swart <mswart@devtation.de>
2016-01-29 01:01:07 +0100

    native: workflow improvement
    * keep build directory
    * use xz instead of gz
    * maintain changelog (add new entry if changelog exists)
    * store dsc + resulting builds (in $gem/$version)
    * remove debug output

--- native.py
+++ native.py
@@ -10,6 +10,7 @@ import shutil
 from datetime import datetime
 from pprint import pprint
 from debian.deb822 import Deb822, Dsc
+from debian.changelog import Changelog
 from dateutil.tz import tzlocal
 
 
@@ -30,13 +31,18 @@ rubies = ['2.2', '2.1']
 
 if not os.path.isdir(gem):
     os.mkdir(gem)
-if not os.path.isfile(os.path.join(gem, version + '.gem')):
+if not os.path.isdir(os.path.join(gem, version)):
+    os.mkdir(os.path.join(gem, version))
+if not os.path.isfile(os.path.join(gem, version, 'orig.gem')):
     subprocess.check_call(['wget',
                            'https://rubygems.org/downloads/{}-{}.gem'.format(gem, version),
-                           '-O', os.path.join(gem, version + '.gem')])
+                           '-O', os.path.join(gem, version, 'orig.gem')])
+    subprocess.check_call(['xz', '--compress', '--verbose', '--keep', '--best', os.path.join(gem, version, 'orig.gem')])
+
+os.chdir(os.path.join(gem, version))
 
 with tempfile.TemporaryDirectory() as d:
-    with tarfile.open(name=os.path.join(gem, version + '.gem')) as t:
+    with tarfile.open(name='orig.gem') as t:
         metadata = gzip.GzipFile(fileobj=t.extractfile('metadata.gz')).read()
         metadata = yaml.load(metadata)
         pprint(metadata)
@@ -45,19 +51,22 @@ with tempfile.TemporaryDirectory() as d:
         own_name = 'debler-rubygem-' + metadata['name']
         if level > 0:
             own_name += '-' + '.'.join(version_parts[:level])
-        own_version = metadata['version']['version'] + '-1'
-        shutil.copyfile(os.path.join(gem, version + '.gem'),
-                        os.path.join(d, '{}_{}.orig.tar'.format(
-                            own_name, metadata['version']['version'])))
-        os.makedirs(os.path.join(d, own_name, 'debian', 'source'))
+        gem_version = metadata['version']['version']
 
-        with open(os.path.join(d, own_name, 'debian', 'source', 'format'), 'w') as f:
+        orig_tar = '{}_{}.orig.tar.xz'.format(own_name,
+                   metadata['version']['version'])
+        if not os.path.islink(orig_tar):
+            os.symlink('orig.gem.xz', orig_tar)
+
+        os.makedirs(os.path.join(own_name, 'debian', 'source'), exist_ok=True)
+
+        with open(os.path.join(own_name, 'debian', 'source', 'format'), 'w') as f:
             f.write("3.0 (quilt)\n")
 
-        with open(os.path.join(d, own_name, 'debian', 'compat'), 'w') as f:
+        with open(os.path.join(own_name, 'debian', 'compat'), 'w') as f:
             f.write("9\n")
 
-        with open(os.path.join(d, own_name, 'debian', 'copyright'), 'w') as f:
+        with open(os.path.join(own_name, 'debian', 'copyright'), 'w') as f:
             f.write(
 """Format: http://dep.debian.net/deps/dep5
 Upstream-Name: {}
@@ -68,19 +77,27 @@ Licence: See LICENCE file
   [LICENCE TEXT]
 """.format(own_name))
 
-        with open(os.path.join(d, own_name, 'debian', 'changelog'), 'w') as f:
-            f.write(
-"""{} ({}) {}; urgency=low
-
-  * Build with debler
-
- -- Malte Swart <packages@devtation.de>  {}
-""".format(own_name, own_version, 'trusty', datetime.now(tz=tzlocal()).strftime('%a, %d %b %Y %H:%M:%S %z')))
+        changelog = os.path.join(own_name, 'debian', 'changelog')
+        if os.path.isfile(changelog):
+            changes = Changelog(file=open(changelog, 'r'))
+            deb_version = changes.get_version()
+            deb_version.debian_revision = str(int(deb_version.debian_revision) + 1)
+            change = 'Rebuild with newer debler'
+        else:
+            changes = Changelog()
+            deb_version = gem_version + '-1'
+            change = 'Build with debler'
+        changes.new_block(package=own_name, version=deb_version,
+                          distributions='trusty', urgency='low',
+                          #change='  * ' + change,
+                          author='Malte Swart <packages@devtation.de>',
+                          date=datetime.now(tz=tzlocal()).strftime('%a, %d %b %Y %H:%M:%S %z'))
+        changes.add_change('\n  * ' + change + '\n')
+        with open(changelog, 'w') as f:
+            changes.write_to_open_file(f)
 
         build_deps = [
             'debhelper',
-            'python3',
-            'python3-yaml',
         ]
         for ruby in rubies:
             build_deps.append('ruby{}'.format(ruby))
@@ -88,7 +105,7 @@ Licence: See LICENCE file
 
         if metadata['name'] in db:
             build_deps.append(db[metadata['name']])
-        control_file = open(os.path.join(d, own_name, 'debian', 'control'), 'wb')
+        control_file = open(os.path.join(own_name, 'debian', 'control'), 'wb')
 
         dsc = Dsc()
         dsc['Source'] = own_name
@@ -170,20 +187,16 @@ Licence: See LICENCE file
                 'dh_install',
                 '-p{package}',
                 'v{v}/*.so',
-                '/usr/lib/${{DEB_BUILD_MULTIARCH}}/ruby/debler-rubygems/{name}/{v}.0/']).format(
+                '/usr/lib/${{DEB_BUILD_MULTIARCH}}/ruby/debler-rubygems/{v}.0/{name}/']).format(
                     v=ruby, name=own_name[15:], package=own_name + '-ruby' + ruby))
 
-        with open(os.path.join(d, own_name, 'debian', 'rules'), 'w') as f:
+        with open(os.path.join(own_name, 'debian', 'rules'), 'w') as f:
             f.write("#!/usr/bin/make -f\n%:\n\tdh $@")
             for target in rules:
                 f.write('\noverride_dh_auto_{target}:\n\t'.format(target=target))
                 f.write('\n\t'.join(rules[target]))
 
-        subprocess.check_call(['gzip', '-1', os.path.join(d, '{}_{}.orig.tar'.format(
-                               own_name, metadata['version']['version']))])
-
-        os.makedirs(os.path.join(d, 'usr', 'lib', 'ruby', 'debler-rubygems', control['Package'][15:]))
-        with open(os.path.join(d, own_name, 'debian', own_name + '.install'), 'w') as f:
+        with open(os.path.join(own_name, 'debian', own_name + '.install'), 'w') as f:
             with tarfile.open(fileobj=t.extractfile('data.tar.gz')) as dt:
                 members = dt.getmembers()
                 for member in members:
@@ -193,18 +206,12 @@ Licence: See LICENCE file
                     else:
                         continue
                     f.write('src/{file} /usr/lib/ruby/debler-rubygems/{name}/{dir}\n'.format(
-                        name=control['Package'][15:],
+                        name=own_name[15:],
                         file=member.name,
                         dir=os.path.dirname(member.name)))
 
-    os.chdir(os.path.join(d, own_name))
-
-    subprocess.call(['find', '.'])
-    subprocess.call(['cat', 'debian/control'])
-    subprocess.call(['cat', 'debian/changelog'])
-    subprocess.call(['cat', 'debian/{}.install'.format(own_name)])
-    subprocess.call(['cat', 'debian/rules'])
+    os.chdir(own_name)
     subprocess.check_call(['dpkg-source', '-b', '.'])
+    os.chdir('..')
 
-    os.chdir(os.path.join(d))
-    subprocess.check_call(['sbuild', '--dist', 'trusty', '{}_{}.dsc'.format(own_name, own_version)])
+    subprocess.check_call(['sbuild', '--dist', 'trusty', '{}_{}.dsc'.format(own_name, deb_version)])
