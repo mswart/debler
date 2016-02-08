2d18cbd597da058e6597b2c7b7e36e8d12e0c51d
Malte Swart <mswart@devtation.de>
2016-02-08 23:24:29 +0100

    gemfile: add basic Gemfile parser
    Add basic lepl based parser for Gemfile. It supports only a
    limit subset of ruby gramma, normally used in Gemfiles.
    
    It parses successfully the xikolo-account Gemfile.
    
    It is WIP, the parse should be combined with the Gemfile.lock parser
    to return all needed information (e.g. also in which group a
    gem is loaded).

--- debler/gemfile.py
+++ debler/gemfile.py
@@ -1,6 +1,92 @@
 from debler.gem import GemVersion
 
 
+def build_str(s):
+    return ''.join(s)
+
+
+class Source():
+    def __init__(self, source):
+        self.source = source
+
+    def __repr__(self):
+        return 'Source({})'.format(self.source)
+
+
+class Gem():
+    def __init__(self, name, constraints, opts):
+        self.name = name
+        self.constraints = constraints
+        self.opts = opts
+        self.opts['envs'] = ['all']
+
+    def __repr__(self):
+        return 'Gem({}, {}, {})'.format(self.name, self.constraints, self.opts)
+
+
+class Group():
+    def __init__(self, envs, *gems):
+        self.envs = envs
+        self.gems = gems
+
+    def __repr__(self):
+        return 'Group({}, {})'.format(self.envs, self.gems)
+
+
+def build_gems(envs, *gems):
+    print(envs, gems)
+    for gem in gems:
+        gem.opts['envs'] = envs
+    return gems
+
+
+def expand(cls):
+    def call(args):
+        return cls(*args)
+    return call
+
+
+def ret(v):
+    def call(arg):
+        return v
+    return call
+
+import lepl
+string = (lepl.Drop('\'') & lepl.Star(lepl.AnyBut('\'')) & lepl.Drop('\'')) > build_str
+symbol = lepl.Drop(':') & lepl.Star(lepl.AnyBut('\':\t ')) > build_str
+
+line_comment = lepl.Literal('#') & lepl.Star(lepl.AnyBut('\n'))
+newline = lepl.Drop(lepl.Star(lepl.Space()) & lepl.Optional(line_comment) & lepl.Literal('\n') & lepl.Star(lepl.Space()))
+
+# define gem line
+gem = lepl.Drop('gem') & ~lepl.Space() & string
+# optional version constraints
+gem_constraints = lepl.Star(~lepl.Drop(',') & ~lepl.Space() & string) > tuple
+gem &= gem_constraints
+# optional keywords
+true = lepl.Literal('true') >> ret(True)
+false = lepl.Literal('false') >> ret(False)
+value = true | false | symbol | string
+keyword_name = lepl.Star(lepl.AnyBut(':\t ')) > build_str
+keyword = keyword_name & lepl.Drop(':') & ~lepl.Space() & value > tuple
+keywords = lepl.Star(~lepl.Drop(',') & ~lepl.Space() & keyword) > dict
+gem &= keywords
+# comment
+gem &= lepl.Drop(lepl.Star(lepl.Space()) & lepl.Optional(line_comment))
+gem = gem > expand(Gem)
+
+# gem group
+group_envs = symbol & lepl.Star(lepl.Drop(',') & ~lepl.Space() & symbol) > tuple
+group_start = ~lepl.Literal('group') & ~lepl.Space() & group_envs & ~lepl.Space() & ~lepl.Literal('do')
+group_content = lepl.Star(gem | newline)
+group_end = ~lepl.Literal('end')
+group = group_start & group_content & group_end > expand(build_gems)
+
+source = ~lepl.Literal('source') & ~lepl.Space() & string > expand(Source)
+
+parser = lepl.Star(source | gem | group | newline)
+
+
 class Parser():
     def __init__(self, file):
         self.parse(file)
