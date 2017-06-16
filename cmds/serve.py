46def934b65d355c5ee4f9d06306e9ab625525b8
Malte Swart <mswart@devtation.de>
2017-06-16 16:48:54 +0200

    server: add simple log + allow post update hook

--- cmds/serve.py
+++ cmds/serve.py
@@ -2,18 +2,27 @@ import functools
 import hashlib
 import http.server
 import json
+import logging
+import subprocess
 
 
 import debler.db
 from debler import config
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)-15s %(name)s %(levelname)s %(message)s',
+    filename='debler-server.log')
+log = logging.getLogger(__name__)
+
 
 class DeblerHandler(http.server.BaseHTTPRequestHandler):
     server_version = 'debler/0.1'
 
-    def __init__(self, db, *args, **kwargs):
+    def __init__(self, args, db, *pargs, **kwargs):
+        self.args = args
         self.db = db
-        super().__init__(*args, **kwargs)
+        super().__init__(*pargs, **kwargs)
 
     def send_data(self, data, content_type='text/plain', response_code=200):
         """ Helper method to send data as HTTP response. The data are
@@ -60,6 +69,7 @@ class DeblerHandler(http.server.BaseHTTPRequestHandler):
             return
         name = data['name']
         version = data['version']
+        kwargs = {'name': name, 'gem': name, 'version': version, 'slot': None}
         if config.rubygems_apikey:
             hashdata = name + version + config.rubygems_apikey
             auth = hashlib.sha256(hashdata.encode('utf-8')).hexdigest()
@@ -67,26 +77,45 @@ class DeblerHandler(http.server.BaseHTTPRequestHandler):
                 self.send_error(403)
                 return
         self.send_data(b'OK')
-        print('{} published in version {}'.format(name, version), end='')
+        log.debug('Webhook triggered for %(gem)s in %(version)s', kwargs)
         info = self.db.gem_info(name, autocreate=False)
         if not info:
-            print('  -> not used')
+            log.debug('Skip release %(version)s of %(gem)s we do not use it',
+                      kwargs)
             return
         new_slot = tuple(int(v) for v in version.split('.')[:info.level])
+        kwargs['slot'] = '.'.join(str(s) for s in new_slot)
         if new_slot in info.slots:
+            new_version = list(int(v) for v in version.split('.'))
+            versions = self.db.gem_slot_versions(name, new_slot)
+            if new_version in versions:
+                log.warning('%(gem)s rerelease in version %(version)s',
+                            kwargs)
+                return
             self.db.create_gem_version(
                 name, list(new_slot),
-                version=list(int(v) for v in version.split('.')), revision=1,
+                version=new_version, revision=1,
                 changelog='New upstream release',
                 distribution=config.distribution)
-            print('  -> scheduled build in slot {}'.format(new_slot))
+            log.info('%(gem)s scheduled to build %(version)s in %(slot)s',
+                     kwargs)
+            if self.args.hook:
+                args = [self.args.hook]
+                for arg in self.args.hook_arg:
+                    args.append(arg.format(**kwargs))
+                log.debug('exec %s', ' '.join(args))
+                subprocess.run(args, check=True, timeout=60)
         else:
-            print('  -> version in unknown slot {}'.format(new_slot))
+            log.info('%(gem)s\'s release %(version)s in unknown slot %(slot)s',
+                     kwargs)
+
+    def log_message(self, format, *args):
+        pass
 
 
 def run(args):
     db = debler.db.Database()
-    connectedHandler = functools.partial(DeblerHandler, db)
+    connectedHandler = functools.partial(DeblerHandler, args, db)
 
     server = http.server.HTTPServer((args.host, args.port), connectedHandler)
     server.allow_reuse_address = True
@@ -100,4 +129,9 @@ def register(subparsers):
                        help='port to listen')
     group.add_argument('--host', default='0.0.0.0',
                        help='host bind for listen socket')
+    parser.add_argument('--hook', help='script to be launch after new deps')
+    parser.add_argument('--hook-arg', action='append', default=[],
+                        help='Argument to hook script; python format is '
+                             'called on each argument, with gem, slot, '
+                             'version keyword arguments')
     parser.set_defaults(run=run, native=None)
