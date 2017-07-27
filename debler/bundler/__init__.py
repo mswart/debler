5c2db7c0dd961d97809e962168cc7dbc608c79bc
Malte Swart <mswart@devtation.de>
2017-07-28 00:43:11 +0200

    bundler: auto-configure gems with default values
    In almost most cases the default values are reused.

--- debler/bundler/__init__.py
+++ debler/bundler/__init__.py
@@ -29,24 +29,10 @@ class BundlerPackager(Packager):
         except ValueError as e:
             if not autocreate:
                 raise e
-            print('Configure {}:'.format(name))
-            from urllib.request import urlopen
-            url = 'https://rubygems.org/api/v1/versions/{}.json'.format(name)
-            data = urlopen(url).read()
-            versions = json.loads(data.decode('utf-8'))
-            for version in versions:
-                print(version['number'] + ' ' + version['created_at'])
-            level = int(input('Level (1): ') or '1')
-            native = {
-                't': True,
-                'f': False,
-                'n': False,
-                'y': True,
-                '': None}[input('Native?: ')]
             self.db.register_pkg(self.id, name, {
                 'default': {
-                    'level': level,
-                    'native': native
+                    'level': 1,
+                    'native': None
                 }
             })
             return self.db.pkg_info(self.id, name, self.name2deb(name))
